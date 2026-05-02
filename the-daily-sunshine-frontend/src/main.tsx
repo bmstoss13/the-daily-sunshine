bab5efdb7c4b9d6d64b685e5792d29f08a44a9c0
Brian Stoss <cyw6gu@virginia.edu>
2026-05-02 15:02:05 -0400

    initialize tanstack router and tanstack query

--- the-daily-sunshine-frontend/src/main.tsx
+++ the-daily-sunshine-frontend/src/main.tsx
@@ -1,10 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen.ts'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 1000 * 60 * 5,
+			retry: 3,
+		}
+	}
+});
+
+const router = createRouter({ 
+	routeTree,
+	context: {
+		queryClient,
+	},
+	defaultPreload: 'intent',
+	defaultPreloadStaleTime: 0,
+});
+
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router
+    }
+}
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<ReactQueryDevtools initialIsOpen={false}/>
+		</QueryClientProvider>
 	</StrictMode>,
 )
