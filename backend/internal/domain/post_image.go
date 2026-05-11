3d8e1bd3e611964e9266edf85b71415b39975376
Brian Stoss <cyw6gu@virginia.edu>
2026-05-10 22:33:51 -0400

    fixed PostImage struct bug. Had DisplayOrder instead of IsCoverImage

--- backend/internal/domain/post_image.go
+++ backend/internal/domain/post_image.go
@@ -8,6 +8,6 @@ type PostImage struct {
 	ImageURL         string    `json:"image_url"`
 	ImageDescription string    `json:"image_description"`
 	AltText          string    `json:"alt_text"`
-	DisplayOrder     int32     `json:"display_order"`
+	IsCoverImage     int32     `json:"is_cover_image"`
 	CreatedAt        time.Time `json:"created_at"`
 }
